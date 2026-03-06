36cdf171d60cf54776f15d2e7ef8b98a4ae7b043
daswits <danielmercadosubirana@gmail.com>
2026-03-06 12:38:23 -0400

    changes for running

--- src/main/java/com/solveria/iamservice/IamServiceApplication.java
+++ src/main/java/com/solveria/iamservice/IamServiceApplication.java
@@ -3,11 +3,21 @@ package com.solveria.iamservice;
 import com.solveria.iamservice.config.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+            MongoAutoConfiguration.class,
+            MongoDataAutoConfiguration.class,
+            RedisAutoConfiguration.class,
+            RedisRepositoriesAutoConfiguration.class
+        })
 @EntityScan(
         basePackages = {
             "com.solveria.core.iam.domain.model",
